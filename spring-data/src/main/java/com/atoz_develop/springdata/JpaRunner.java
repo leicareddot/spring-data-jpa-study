e692f6a7239fd32a370e9a992eec81f15febc071
leicareddot <leicareddot@gmail.com>
2020-03-14 20:52:44 +0900

    JPA 프로그래밍 - Query Native query

--- spring-data/src/main/java/com/atoz_develop/springdata/JpaRunner.java
+++ spring-data/src/main/java/com/atoz_develop/springdata/JpaRunner.java
@@ -1,17 +1,11 @@
 package com.atoz_develop.springdata;
 
-import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -24,12 +18,7 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Post> query = builder.createQuery(Post.class);
-        Root<Post> root = query.from(Post.class);
-        query.select(root);
-
-        List<Post> posts = entityManager.createQuery(query).getResultList();
+        List<Post> posts = entityManager.createNativeQuery("Select * from Post", Post.class).getResultList();
         posts.forEach(System.out::println);
     }
 }
